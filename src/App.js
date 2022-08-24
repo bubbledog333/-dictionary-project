d40803fb6bad671793fbb92d4cd0736685e7c854
bubbledog333 <95395175+bubbledog333@users.noreply.github.com>
2022-08-23 17:52:50 -1000

    initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <h1>-Dictionary project-</h1>
+      <p>Form , input , boostrap, and ect.</p>
+      <a href="l" class="btn btn-dark shadow">Button</a>
+     <br/> <form>
+        <input placeholder="What to look up?"/>
+      </form>
+       
+      </header>
+    </div>
+  );
+}
+
+export default App;
