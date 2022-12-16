b8c884d79020e10a0774f2e7f391c853efc3e1b7
bubbledog333 <95395175+bubbledog333@users.noreply.github.com>
2022-12-15 15:56:49 -1000

    me

--- src/Results.js
+++ src/Results.js
@@ -1,18 +1,28 @@
 import React from "react"
-import Meaning from "./Meaning.js"
+import Meaning from "./Meaning.js";
+import Phonetic from "./Phonetic";
 import "./App.css"
 export default function Result(props){
-    console.log(props.results)
  if (props.results) {
     return(
         <div className="Result">
           <h2>{props.results.word}</h2>  
+          {props.results.phonetics.map(function(phonetic, index){
+          return (
+          <div key={index}>
+           <Phonetic phonetic={phonetic} />
+          </div>
+          )
+          })}
         {props.results.meanings.map(function(meaning, index){
-           return (<div key={index}>
-<Meaning  meaning={meaning}/>
-           </div>);
+           return (
+           <div key={index}>
+        <Meaning  meaning={meaning}/>
+           </div>
+           );
         })}
-        </div>)
+        </div>
+        )
         
     } else{
         return null;
--- src/Synonyms.js
+++ src/Synonyms.js
@@ -1,6 +1,7 @@
 import React from "react"
 import './App.css'
 export default function Synonyms(props){
+ 
     if (props.synonyms){
    return (
    <div className="Synonyms"> <ul classame="synonyms">
